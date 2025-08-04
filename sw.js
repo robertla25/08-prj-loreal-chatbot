7a8c9a072fc8c3f03d9e73d7f1e6f16db4929f39
Robert Alamo <93059230+robertla25@users.noreply.github.com>
2025-08-04 18:46:48 +0000

    Mobile web app updates

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Minimal service worker for PWA installability
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
+});
